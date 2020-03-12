029d9a13e1cecf1619b0f20b9bdf62d84ea9205e
Emo <eiv.razgrad@gmail.com>
2020-03-12 15:30:58 +0200

    add joke properties and views on home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,40 @@
 <template>
-  <div class="hello">
+  <div class="container">
     <h1>{{ msg }}</h1>
     <p>
-      Ние разполагаме с богата колекция от {{count}} виц/ове, които ще ви харесат,
-      гарантирано ;)
-      <br />Разполагате и с възможност да се регистрирате и да създадете
-      собствен виц.
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
+      бележки:
+      Трябва още: - регистрация - логин - логоут - изглед за логнати - не логнати
+      - връзка с база данни - за нас - контакти - логиката за добавяне на вицове - оформление;
+
+      Ние разполагаме с богата колекция от {{ count }} виц/ове, които ще ви
+      харесат, гарантирано ;) <br />Разполагате и с възможност да се
+      регистрирате и да създадете собствен виц.
     </p>
+    <hr class="hrTitle" size="30" />
+    <div v-for="(j, i) in jokes_bank" class="gallery" :key="i">
+      <div>
+        <h2>{{ j.title }}</h2>
+        <h5>Категория: {{ j.category }}</h5>
+      </div>
+      <div class="desc">
+        <template v-if="j.showDescription">
+          <p>
+            {{ j.description }}
+          </p>
+          <p><b>Дата:</b>{{ j.date }}</p>
+          <p><b>Автор:</b>{{ j.author }}</p>
+          <button @click="j.showDescription = false" class="show-desc">
+            Show Less
+          </button>
+        </template>
+        <template v-else>
+          <button @click="j.showDescription = true" class="show-desc">
+            Show more
+          </button>
+        </template>
+        <hr class="hrJokes" size="3"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,14 +48,31 @@ export default {
     return {
       jokes_bank: [
         {
-          title: 'Ivancho',         
-          description: "Иванчо говори с баща си Татко,ти си голям късметлия!- Защо?- Няма нужда да ми купуваш нови учебници, оставам в същия клас!",
-          author: 'Емануил',
+          category: "Ivancho",
+          title: "Ivancho && Father",
+          description:
+            "Иванчо говори с баща си Татко,ти си голям късметлия!- Защо?- Няма нужда да ми купуваш нови учебници, оставам в същия клас!",
+          author: "Емануил",
+          date: new Date(),
+          showDescription: false
+        },
+        {
+          category: "Mariika",
+          title: "Mariika && Mother",
+          description:
+            "Lorem ipsum е един от най-често използваните в печатарството и графичния дизайн заготовъчни текстове, служещи да запълват със съдържание онези графични елементи на",
+          author: "Petyr",
           date: new Date(),
           showDescription: false
         }
       ]
     };
+  },
+  methods: {
+    setJokeDescription(e) {
+      const value = e.target.value;
+      this.imgDescription = value;
+    }
   },
   computed: {
     count() {
@@ -42,6 +84,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+hr.hrTitle {
+  height: 3px;
+  color: red;
+  background-color: red;
+  border: none;
+}
+hr.hrJokes {
+  height: 2px;
+  color: rgba(25, 0, 255, 0.555);
+  background-color: blue;
+  border: none;
+}
 h3 {
   margin: 40px 0 0;
 }
