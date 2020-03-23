9e1d63edf32a1d3da5b2294b7a891f14ea1502a8
Emo <eiv.razgrad@gmail.com>
2020-03-23 15:34:47 +0200

    add Register.vue - validation, v-model, error and so on.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,20 +6,69 @@
         <a href="register.html" class="active-route">Register</a>
       </h2>
 
-      <form class="user-form">
+      <form @submit.prevent="submitHandler" class="user-form">
         <div class="form-group">
-          <img src="https://img.icons8.com/material-sharp/42/000000/user.png" />
-          <input type="text" name="username" placeholder="Username" />
+          <label for="username">
+            <img
+              src="https://img.icons8.com/material-sharp/42/000000/user.png"
+            />
+          </label>
+          <input
+            id="username"
+            type="text"
+            name="username"
+            placeholder="Username"
+            v-model="username"
+          />
+          <template v-if="$v.username.$error">
+            <div v-if="!$v.username.required">Username is requerd!</div>
+            <div v-if="!$v.username.minLength">
+              Username should be more than 3 symbols!
+            </div>
+          </template>
         </div>
 
         <div class="form-group">
-          <img src="https://img.icons8.com/material/42/000000/password--v1.png" />
-          <input type="password" name="password" placeholder="Password" />
+          <label for="password">
+            <img
+              src="https://img.icons8.com/material/42/000000/password--v1.png"
+            />
+          </label>
+
+          <input
+            id="password"
+            type="password"
+            name="password"
+            placeholder="Password"
+            v-model="password"
+          />
+          <template v-if="$v.password.$error">
+            <div v-if="!$v.password.required">Password is requerd!</div>
+            <div v-if="!$v.password.minLength">
+              Password should be more than 3 symbols!
+            </div>
+          </template>
         </div>
 
         <div class="form-group">
-          <img src="https://img.icons8.com/material/42/000000/password--v1.png" />
-          <input type="password" name="rePassword" placeholder="Repeat password" />
+          <label for="rePassword">
+            <img
+              src="https://img.icons8.com/material/42/000000/password--v1.png"
+            />
+          </label>
+
+          <input
+            id="rePassword"
+            type="password"
+            name="rePassword"
+            placeholder="Repeat password"
+            v-model="rePassword"
+          />
+          <template v-if="$v.rePassword.$error">
+            <div v-if="!$v.rePassword.sameAs">
+              Password dont match!
+            </div>
+          </template>
         </div>
 
         <button>Register</button>
@@ -29,13 +78,52 @@
 </template>
 
 <script>
-import AppContent from './shared/Content';
+import AppContent from "./shared/Content";
+import { validationMixin } from "vuelidate";
+import { required, minLength } from "vuelidate/lib/validators";
+
+function sameAs(field) {
+  return function(value) {
+    return this[field] === value;
+  };
+}
+
 export default {
+  mixins: [validationMixin],
   components: {
     AppContent
+  },
+  methods: {
+    submitHandler() {
+      this.$v.$touch();
+
+      if (this.$v.$invalid) {
+        return;
+      }
+      console.log("Form was submitted!");
+    }
+  },
+  data() {
+    return {
+      username: "",
+      password: "",
+      rePassword: ""
+    };
+  },
+  validations: {
+    username: {
+      required,
+      minLength: minLength(5)
+    },
+    password: {
+      required,
+      minLength: minLength(8)
+    },
+    rePassword: {
+      sameAs: sameAs("password")
+    }
   }
-}
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
