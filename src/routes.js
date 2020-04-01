597d965550d17dca5fc51cba950ce62f8251949d
Emo <eiv.razgrad@gmail.com>
2020-04-01 11:04:19 +0300

    add NotFound.vue, edit route to redirect in home page without address

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,24 @@
-
-import Login from './components/Login.vue';
-import Home from './components/Home.vue';
-import Test from './components/Test.vue';
-import Register from './components/Register.vue';
-import Createjoke from './components/CreateJoke.vue';
-import Jokesites from './components/JokeSites.vue';
-
+import Login from "./components/Login.vue";
+import Home from "./components/Home.vue";
+import Test from "./components/Test.vue";
+import Register from "./components/Register.vue";
+import Createjoke from "./components/CreateJoke.vue";
+import Jokesites from "./components/JokeSites.vue";
+import NotFound from "./components/NotFound.vue";
 
 export default [
-    { path: '/login', component: Login },
-    { path: '/createjoke', component: Createjoke },
-    { path: '/Jokesites', component: Jokesites },
-    { path: '/home', component: Home },
-    { path: '/test', component: Test },
-    { path: '/register', component: Register }
-]
+  { path: "/login", component: Login },
+  { path: "/createjoke", component: Createjoke },
+  { path: "/Jokesites", component: Jokesites },
+  { path: "/home", component: Home },
+  { path: "/test", component: Test },
+  { path: "/register", component: Register },
+  {
+    path: "*",
+    component: NotFound
+  },
+  {
+    path: "/",
+    component: Home
+  }
+];
